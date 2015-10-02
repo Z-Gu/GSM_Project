c236e3ccd40897da42f2619e09884d56545b347a
Z-Gu <z_gu@outlook.com>
2015-10-02 16:30:43 +0100

    More Comment

--- GSM_Direct/sensor.py
+++ GSM_Direct/sensor.py
@@ -2,10 +2,15 @@ from subprocess import call
 
 def get(lens = "Dummy"):
 
+        #call the scanner, store as file
+
 	file = lens + '_iso'
 	tmp = open(file, 'w')
 	call(["/root/scannerapi/scanner/scan_iso", "-s", lens], stdout = tmp)
 	tmp.close()
+
+        #Hexdump the binary to HEX string
+
 	tmp_ = open(file + '_', 'w')
 	call(["hexdump", "-e", '16/1 \"%02x\"', file], stdout = tmp_)
 	tmp_.close()
